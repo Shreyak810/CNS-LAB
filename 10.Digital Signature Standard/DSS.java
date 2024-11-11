670bfe61f73f25c8496812c441ec8872169c7cee
Shreya Khambe <113600207+Shreyak810@users.noreply.github.com>
2024-11-11 20:10:21 +0530

    Create DSS.java

---
+++ 10.Digital Signature Standard/DSS.java
@@ -0,0 +1,61 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.util.Scanner;
+
+public class DSS {
+
+    private static final String SIGNING_ALGORITHM = "SHA256withRSA";
+    private static final String RSA = "RSA";
+    private static Scanner sc;
+
+    public static byte[] Create_Digital_Signature(byte[] input, PrivateKey Key) throws Exception {
+        Signature signature = Signature.getInstance(SIGNING_ALGORITHM);
+        signature.initSign(Key);
+        signature.update(input);
+        return signature.sign();
+    }
+
+    public static KeyPair Generate_RSA_KeyPair() throws Exception {
+        SecureRandom secureRandom = new SecureRandom();
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance(RSA);
+        keyPairGenerator.initialize(2048, secureRandom);
+        return keyPairGenerator.generateKeyPair();
+    }
+
+    public static boolean Verify_Digital_Signature(byte[] input, byte[] signatureToVerify, PublicKey key) throws Exception {
+        Signature signature = Signature.getInstance(SIGNING_ALGORITHM);
+        signature.initVerify(key);
+        signature.update(input);
+        return signature.verify(signatureToVerify);
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder(2 * bytes.length);
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xff & bytes[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString().toUpperCase();
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        String input = "WCE Sangli";
+        KeyPair keyPair = Generate_RSA_KeyPair();
+
+        byte[] signature = Create_Digital_Signature(input.getBytes(), keyPair.getPrivate());
+
+        System.out.println("Signature Value:\n " + bytesToHex(signature));
+
+        System.out.println("Verification: " + Verify_Digital_Signature(input.getBytes(), signature, keyPair.getPublic()));
+    }
+}
+
+
